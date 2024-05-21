383681dfb274699ec9ffae07b56fa9952f82ddf3
rishav <1729rishavranjan@gmail.com>
2024-05-22 00:51:42 +0530

    Updated file

--- 01basics/test.py
+++ 01basics/test.py
@@ -6,3 +6,13 @@ print("""use triple quotes for
 multiple lines""")
 
 print('I\'m learning python') # \ is used as to escape the next character so in this case single qoute is accepted as it is without having any extra meaning.
+
+my_var = "barks"
+print("A dog {my_var}")
+print(f"A dog {my_var}") # when we use f string it will consider anything within a {} as an expression
+print("A dog {}".format(my_var)) 
+
+print("Hello \t world") # \t is uesd for tab space
+
+print("C:\\user\\test") #if you want to print \ you have to use double \ for that
+print("C:\user\test")# this will throw SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 2-3: truncated \uXXXX escape
