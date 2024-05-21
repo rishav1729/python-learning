67244e22d81b7a0cf2876319afafeaffacc91f50
rishav <1729rishavranjan@gmail.com>
2024-05-22 00:37:37 +0530

    first commit

---
+++ 01basics/test.py
@@ -0,0 +1,8 @@
+print("4",7) #print seperately
+
+print(type("12"))
+
+print("""use triple quotes for 
+multiple lines""")
+
+print('I\'m learning python') # \ is used as to escape the next character so in this case single qoute is accepted as it is without having any extra meaning.
