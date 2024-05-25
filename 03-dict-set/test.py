c329ee5c4ffd65aeab443604500e4edcc941226b
rishav <1729rishavranjan@gmail.com>
2024-05-25 21:19:06 +0530

    about dictionary

---
+++ 03-dict-set/test.py
@@ -0,0 +1,8 @@
+student_deteails ={
+    'Henry': 1999,
+    'Ramu': 2001,
+    'Jacob': 2002,
+    'Ramu' : 2000
+}
+
+print(student_deteails) # If the same key is used in dict. then the latter key's value will overwrite the previoues one at last the dict. will have onle one key, so key is unique.
