2e7ec42e910fd036152b7ca4c85a032cafd89d06
rishav <1729rishavranjan@gmail.com>
2024-05-27 23:27:47 +0530

    update

--- 03-dict-set/test.py
+++ 03-dict-set/test.py
@@ -1,8 +1,49 @@
-student_deteails ={
-    'Henry': 1999,
-    'Ramu': 2001,
-    'Jacob': 2002,
-    'Ramu' : 2000
-}
+# student_deteails ={
+#     'Henry': 1999,
+#     'Ramu': 2001,
+#     'Jacob': 2002,
+#     'Ramu' : 2000
+# }
 
-print(student_deteails) # If the same key is used in dict. then the latter key's value will overwrite the previoues one at last the dict. will have onle one key, so key is unique.
+# print(student_deteails) # If the same key is used in dict. then the latter key's value will overwrite the previoues one at last the dict. will have onle one key, so key is unique.
+
+
+# # set
+
+# set1 = {'cpp', 'java', 'python', 'javascript'}
+
+# set2 = set1
+
+# set2.add('sql') # since set is unordered list sql element will be add in the set at random index
+# print (set1)
+# print(set2)
+
+# #set2[2] #TypeError: 'set' object is not subscriptable, as it is unordered list elements cannot be accessed by its index.
+
+# str= "str"
+# str = "string"
+# print(str)
+
+# print(10=='10')
+
+# arr= ['one', 'two', 'three']
+
+# a,b,c = arr
+
+# print(a,b,c)
+
+
+
+
+
+
+def show_name(arr_of_names):
+    [name1,name2,_]=arr_of_names
+   
+    print(name1,name2)
+
+
+
+show_name(["aditya","rishab","chandan"])
+
+
