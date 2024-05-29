861234e04b215c7a127abcea4f8ce013062fa474
rishav <1729rishavranjan@gmail.com>
2024-05-29 22:39:57 +0530

    if-else updated

--- 04-if-else/test.py
+++ 04-if-else/test.py
@@ -1,12 +1,16 @@
-sentence = input("Enter sone white space seperaated words:")
+# sentence = input("Enter sone white space seperaated words:")
 
-words = sentence.split(" ")
-print(words) #['what', 'lies', 'behind', 'us', 'and', 'what', 'lies', 'before', 'us', 'are', 'tiny', 'matters', 'ccompared', 'to', 'what', 'lies', 'within', 'us']
+# words = sentence.split(" ")
+# print(words) #['what', 'lies', 'behind', 'us', 'and', 'what', 'lies', 'before', 'us', 'are', 'tiny', 'matters', 'ccompared', 'to', 'what', 'lies', 'within', 'us']
 
-set_of_words = set(words)
-print(set_of_words) #{'and', 'behind', 'to', 'matters', 'ccompared', 'what', 'lies', 'within', 'are', 'tiny', 'before', 'us'}
+# set_of_words = set(words)
+# print(set_of_words) #{'and', 'behind', 'to', 'matters', 'ccompared', 'what', 'lies', 'within', 'are', 'tiny', 'before', 'us'}
 
-sorted_set_of_words = sorted(set_of_words)
-print(sorted_set_of_words) #['and', 'are', 'before', 'behind', 'ccompared', 'lies', 'matters', 'tiny', 'to', 'us', 'what', 'within']
+# sorted_set_of_words = sorted(set_of_words)
+# print(sorted_set_of_words) #['and', 'are', 'before', 'behind', 'ccompared', 'lies', 'matters', 'tiny', 'to', 'us', 'what', 'within']
 
-print(" ".join(sorted_set_of_words)) #and are before behind ccompared lies matters tiny to us what within
+# print(" ".join(sorted_set_of_words)) #and are before behind ccompared lies matters tiny to us what within
+
+words = ["Hello", 10 ,"world"]
+sentence = " ".join(words)  
+print(sentence)
